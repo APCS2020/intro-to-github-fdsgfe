c6af6e1a0d576f2a0f6dd772ae72fefdb74c12d8
David张宇健 <50470541+fdsgfe@users.noreply.github.com>
2019-05-17 10:38:01 +0800

    Add files via upload

---
+++ david.java
@@ -0,0 +1,13 @@
+public class david{
+    public static void main(String[] args){
+        for(int i = 0; i < 3; i++){
+            for (int j = i; j < 2; j++){
+                System.out.print("*");
+            }
+            for (int h = 0; h < i*2+1; h++){
+                System.out.print("A");
+            }
+            System.out.println();
+        }
+    }
+}
